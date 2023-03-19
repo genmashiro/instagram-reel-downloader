e677ac921513c55a9d5c83765f4e5968a3d62590
Gen <deutscherzocker353@gmail.com>
2023-03-19 14:22:08 +0100

    Update v0.0.3
    - performance fixes
    - bug fixes
    - removed threading
    - added concurrent.futures

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import instaloader
-import threading
+import concurrent.futures
 import PySimpleGUI as sg
 from pathlib import Path
 
@@ -7,18 +7,25 @@ from pathlib import Path
 L = instaloader.Instaloader()
 
 # Define function to download reel
-def download_reels(values):
-    reel_urls = values['-URLS-'].split('\n')
-    for url in reel_urls:
-        t = threading.Thread(target=download_reel, args=(url,))
-        t.start()
-
 def download_reel(reel_url):
     try:
         post = instaloader.Post.from_shortcode(L.context, reel_url.split("/")[-2])
         L.download_post(post, target=post.owner_username)
+        return True
     except Exception as e:
         print(f"Error downloading reel: {e}")
+        return False
+
+def download_reels(values):
+    reel_urls = values['-URLS-'].split('\n')
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        futures = [executor.submit(download_reel, url) for url in reel_urls]
+        results = [future.result() for future in futures]
+
+    if all(results):
+        sg.Popup('Success', 'All reels downloaded successfully.')
+    else:
+        sg.Popup('Error', 'One or more reels failed to download.')
 
 # Define GUI layout
 layout = [[sg.Text('Enter Instagram Reel URLs (one URL per line):')],
@@ -35,7 +42,6 @@ while True:
         break
     elif event == 'Download Reels':
         download_reels(values)
-        sg.Popup('Success', 'All reels downloaded successfully.')
 
 # Close GUI window
 window.close()
